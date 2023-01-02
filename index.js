c192534e4893eafca8578acc9086c589face1a45
Tyler Walton <tylerwalton300@gmail.com>
2023-01-02 15:10:00 -0500

    created post get and listen routes for server

--- index.js
+++ index.js
@@ -6,4 +6,31 @@ const { v4: uuid } = require('uuid');
 
 const app = express();
 
+app.use(express.json());
+
+app.get("/outfit", (req, res) => {
+    const tops = ["Black", "White", "Navy", "OD Green"];
+    const jeans = ["Black", "Grey", "Navy", "Dark Grey"];
+    const shoes = ["Black", "Grey", "White"];
+      
+    res.json({
+        top: _.sample(tops),
+        jeans: _.sample(jeans),
+        shoes: _.sample(shoes)
+    });
+});
+
+app.post("/comments", (req, res) => {
+    const id = uuid();
+    const content = req.body.content;
+
+    if (!content) {
+        return res. sendStatus(400);
+    }
+
+    await fs.mkdir('data/comments', { recursive: true });
+    
+    res.sendStatus(201);
+});
+
 app.listen(3000, () => console.log("API server is running..."));
